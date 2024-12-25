4184528285d1d378785c30b1c8460e0b991d91b7
ryznscripts <3mkroses@gmail.com>
2024-12-25 22:42:08 +0400

    Update redzhub.lua

--- redzhub.lua
+++ redzhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://darkscripts.space/scripts/9ca9f988-97da-4ca0-8264-e863b5bdeda7_879039631373520979.lua',true))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/7ed01b24-f15a-4c28-9cfb-f5b64b4a461a_879039631373520979.lua',true))()
